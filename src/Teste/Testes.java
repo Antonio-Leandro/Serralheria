02ef29491eae6ac2ae6d7b4927a4942e409c6777
Antonio-Leandro <aleandrosaldanha@gmail.com>
2021-03-02 15:52:03 -0300

    Teste

--- src/Teste/Testes.java
+++ src/Teste/Testes.java
@@ -9,7 +9,7 @@ public class Testes {
 	public static void main(String[] args) throws SQLException {
 
 		Pessoa pessoa = new Pessoa();
-		pessoa.setCPF("164.881.778-58");
+		pessoa.setCPF("164.881.778-59");
 		pessoa.setNOME("Antonio Saldanha");
 		pessoa.setRG("24.259.699-X");
 		pessoa.setDATA_DE_NASCIMENTO("19/05/1976");
