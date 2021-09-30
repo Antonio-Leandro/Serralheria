2d3f2b222ceebb448a6ea6c499d9ee5f95518769
Antonio-Leandro <61568166+Antonio-Leandro@users.noreply.github.com>
2021-09-30 18:59:48 -0300

    Ajustar classe para versão 8.0 do MySQL.

--- src/Factory/ConnectionFactory.java
+++ src/Factory/ConnectionFactory.java
@@ -7,16 +7,16 @@ import java.sql.Statement;
 
 
 public class ConnectionFactory {
-    public Statement stmt;
-    public ResultSet rs;
+    //public Statement stmt;
+    //public ResultSet rs;
     public Connection getConnection() {
         try {
-            //System.out.println("Conectando no banco de dados...");
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/serralheria?serverTimezone=UTC", "root", "antonio");     
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/serralheria", "root", "antonio");
+            //return DriverManager.getConnection("jdbc:mysql://localhost:3306/serralheria?serverTimezone=UTC", "root", "antonio");
         } catch (Exception e) {
-        	System.out.println("Erro de conexão com o banco de dados..." + e.getMessage());
-                e.printStackTrace();
-                return null;
+            System.out.println("Erro de conexão com o banco de dados..." + e.getMessage());
+            e.printStackTrace();
+            return null;
         }
     }
 }
