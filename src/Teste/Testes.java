11b458ab5827c8cee4214e71885ed05214ad1f59
Antonio-Leandro <61568166+Antonio-Leandro@users.noreply.github.com>
2021-10-01 14:53:21 -0300

    Ajuste campos da tabela tbl_pessoa.

--- src/Teste/Testes.java
+++ src/Teste/Testes.java
@@ -9,15 +9,15 @@ public class Testes {
 
 	public static void main(String[] args) throws SQLException {
 
-		/*Pessoa pessoa = new Pessoa();
+		Pessoa pessoa = new Pessoa();
 		pessoa.setCPF("510.427.083-07");
-		pessoa.setNOME("Antonio Saldanha");
+		pessoa.setNOME("Antonio Leandro Saldanha");
 		pessoa.setRG("24.259.699-X");
 		pessoa.setDATA_DE_NASCIMENTO("19/05/1976");
-		pessoa.setNOME("Antonio Leandro Saldanha");
-		pessoa.setRG("24.259.699-0");
-		pessoa.setDATA_DE_NASCIMENTO("19/05/1975");
-		pessoa.setDATA_DE_CADASTRO("25/01/2021");
+		//pessoa.setNOME("Antonio Leandro Saldanha");
+		//pessoa.setRG("24.259.699-0");
+		//pessoa.setDATA_DE_NASCIMENTO("19/05/1975");
+		pessoa.setDATA_DE_CADASTRO("01/10/2021");
 		pessoa.setNACIONALIDADE("Brasileiro");
 		pessoa.setNATURALIDADE("Ceará");
 		pessoa.setESTADO_CIVIL("Solteiro");
@@ -40,8 +40,8 @@ public class Testes {
 		pessoa.setCIDADE_END_COM("São Paulo");
 		pessoa.setESTADO_END_COM("SP");
 		pessoa.setTIPO_PESSOA("C");
-                pessoa.setLOGIN("Antonio");
-                pessoa.setSENHA("1234");
+                pessoa.setLOGIN("");
+                pessoa.setSENHA("");
 
 		PessoaDao pessoadao = new PessoaDao();
 		try {
@@ -53,7 +53,7 @@ public class Testes {
 		    }
 		} catch (SQLException ex) {
 			System.out.println("Não foi possível salvar o cadastro..." + ex.getMessage());
-		}*/
+		}
                 
                 /*try {
 		    if (pessoadao.busca(pessoa)) {
@@ -89,8 +89,8 @@ public class Testes {
                 
                 //pessoadao.listar();
                 
-                ConnectionFactory connection = new ConnectionFactory();
-                connection.getConnection();
+                /*ConnectionFactory connection = new ConnectionFactory();
+                connection.getConnection();*/
 	}
         
 }
