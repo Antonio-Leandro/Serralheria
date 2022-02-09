bc58dd8b2da1898e100249a5ee572238612257b0
Limitado - 3584 <Limitado - 3584@DESKTOP-3239IUI>
2022-02-09 17:06:49 -0300

    Removidas as importações não usadas.

--- src/Factory/ConnectionFactory.java
+++ src/Factory/ConnectionFactory.java
@@ -2,17 +2,14 @@ package Factory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 
 public class ConnectionFactory {
-    //public Statement stmt;
-    //public ResultSet rs;
+   
     public Connection getConnection() {
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/serralheria", "root", "antonio");
-            //return DriverManager.getConnection("jdbc:mysql://localhost:3306/serralheria?serverTimezone=UTC", "root", "antonio");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/serralheria", "root", "Alean045");
+            //return DriverManager.getConnection("jdbc:mysql://localhost:3306/serralheria?serverTimezone=UTC", "root", "Alean045");
         } catch (Exception e) {
             System.out.println("Erro de conexão com o banco de dados..." + e.getMessage());
             e.printStackTrace();
